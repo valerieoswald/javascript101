32947a396e2c8a598c109b493aa3d22e4e90a812
Valerie Oswald <valerie.oswald@student.uibk.ac.at>
2018-04-11 17:28:28 +0200

    git log

---
+++ main.js
@@ -0,0 +1,31 @@
+// alert ("Hello World");
+
+let nachricht= "Hallo Welt";
+//alert (nachricht);
+nachricht= "neuer Text";
+//alert (nachricht);
+console.log(nachricht);
+const linkColor = "#ff000";
+let eine_variable = "test";
+console.log("Variable: eine_Variable", eine_variable);
+let highScore = 75.9944;
+console.log("Highscore ist:", highScore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = "test2";
+console.log(meineNachricht);
+meineNachricht= `Ein mehr
+und ein
+ganz langer
+text
+und aus`;
+
+console.log(meineNachricht);
+
+let fullName= 'Jeffrey "The Dude" Lebowsky';
+console.log(fullName);
+
+let isOver18= true;
+console.log("über 18?", isOver18);
+let age= 19
+console.log("age über 18", age > 18);
